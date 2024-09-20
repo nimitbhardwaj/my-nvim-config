7f967eba90b4257ef672928d54ed677ce69b2592
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2024-09-20 15:45:51 +0530

    Undo change

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,23 +2,4 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config=function()
-        require("telescope").setup({
-            defaults = {
-              vimgrep_arguments = {
-                'rg',
-                '--color=never',
-                '--no-heading',
-                '--with-filename',
-                '--line-number',
-                '--column',
-                '--smart-case',
-                '--highlight'
-              },
-              -- Highlight matched terms
-              grep_highlight = true,
-              -- Other Telescope configurations...
-            }
-        })
-    end
 }
