5ef36c62ec28ce0e298dd6d13303ea3f2f289ec4
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2024-09-20 15:39:52 +0530

    Enhance Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,24 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config=function()
+        require("telescope").setup({
+            defaults = {
+              vimgrep_arguments = {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case',
+                '--highlight'
+              },
+              -- Highlight matched terms
+              grep_highlight = true,
+              -- Other Telescope configurations...
+            }
+        })
+    end
 }
