40b422eed89bb6e89df16a002062dbd75748991b
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2024-09-21 21:00:14 +0530

    Configure autocomplete and luasnippet together

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -6,7 +6,8 @@ return {
       'hrsh7th/cmp-buffer',    -- Buffer completions
       'hrsh7th/cmp-path',      -- Path completions
       'hrsh7th/cmp-cmdline',   -- Command line completions
-      'L3MON4D3/LuaSnip'       -- Snippet completions
+      'L3MON4D3/LuaSnip',       -- Snippet completions
+      'saadparwaiz1/cmp_luasnip'
     },
     config = function()
       local cmp = require('cmp')
@@ -21,26 +22,33 @@ return {
         },
         mapping = {
           -- Trigger completion manually and also use C-N and C-P for navigation and trigger
-          ['<C-Space>'] = cmp.mapping.complete(),  -- Manually trigger with Ctrl-Space
           ['<C-n>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()  -- If the completion is visible, go to the next item
+              cmp.select_next_item()  -- If the completion is visible, go to the previous item
             else
-              cmp.complete()  -- If not visible, open the completion menu
+              cmp.complete({
+                config={sources={{name='buffer'}, {name='nvim_lsp'}}}
+              })
             end
           end, { 'i', 'c' }),
           ['<C-p>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()  -- If the completion is visible, go to the previous item
             else
-              cmp.complete()  -- If not visible, open the completion menu
+              cmp.complete({
+                config={sources={{name='buffer'}, {name='nvim_lsp'}}}
+              })
             end
           end, { 'i', 'c' }),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- Confirm and select the current item
+          ['<C-k>'] = cmp.mapping(function(fallback)
+            cmp.complete({ config = { sources = { { name = 'luasnip' } } } })  -- Show only snippets
+          end, {'i', 'c'}),
         },
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },  -- LSP completions
           { name = 'buffer' },    -- Buffer completions
+          { name = 'luasnip' },    -- Buffer completions
         })
       })
     end
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,11 +1,14 @@
 return {
     "L3MON4D3/LuaSnip",
+    dependencies={
+        "rafamadriz/friendly-snippets",
+    },
     version="v2.*",
     event="VeryLazy",
     run="make install_jsregexp",
     config=function()
         require('luasnip').setup({})
-        vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+        require("luasnip.loaders.from_vscode").lazy_load()
         vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
 
