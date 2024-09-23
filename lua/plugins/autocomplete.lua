4e9c09029e780ef7d576ee9eca2c20e6027bd0db
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2024-09-23 07:28:54 +0530

    Support automatic autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -11,44 +11,34 @@ return {
     },
     config = function()
       local cmp = require('cmp')
+
+      local lsp_cfg = {name='nvim_lsp'}
+      local buffer_cfg = {name='buffer'}
+      local luasnip_cfg = {name="luasnip"}
+
       cmp.setup({
-        completion={
-          autocomplete=false
-        },
         snippet = {
           expand = function(args)
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users
           end,
         },
         mapping = {
-          -- Trigger completion manually and also use C-N and C-P for navigation and trigger
           ['<C-n>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()  -- If the completion is visible, go to the previous item
-            else
-              cmp.complete({
-                config={sources={{name='buffer'}, {name='nvim_lsp'}}}
-              })
             end
           end, { 'i', 'c' }),
           ['<C-p>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_prev_item()  -- If the completion is visible, go to the previous item
-            else
-              cmp.complete({
-                config={sources={{name='buffer'}, {name='nvim_lsp'}}}
-              })
+              cmp.select_prev_item()
             end
           end, { 'i', 'c' }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- Confirm and select the current item
-          ['<C-k>'] = cmp.mapping(function(fallback)
-            cmp.complete({ config = { sources = { { name = 'luasnip' } } } })  -- Show only snippets
-          end, {'i', 'c'}),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
         },
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' },  -- LSP completions
-          { name = 'buffer' },    -- Buffer completions
-          { name = 'luasnip' },    -- Buffer completions
+            lsp_cfg,
+            buffer_cfg,
+            luasnip_cfg
         })
       })
     end
