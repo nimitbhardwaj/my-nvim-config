5a8dc6216c38e4074ae1b3c545cef0cc2d2eff83
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2024-11-21 19:19:21 +0530

    Make lualine status global

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,12 +1,12 @@
 return {
     "nvim-lualine/lualine.nvim",
-    name="statusline",
-    config=function()
+    name = "statusline",
+    config = function()
         require("lualine").setup({
-            options={
-                theme="dracula",
-                disabled_filetypes={"NvimTree"}
-
+            options = {
+                theme = "dracula",
+                disabled_filetypes = { "NvimTree" },
+                globalstatus = true
             }
         })
     end
