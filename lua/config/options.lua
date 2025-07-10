f9bd0d758101cc3edb29015968b5f377254a42b0
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2025-07-10 08:34:05 +0530

    Add line wraps

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
+
+-- Wrap text
+vim.wo.wrap = true -- Enable line wrapping
+vim.wo.linebreak = true -- Wrap only at word boundaries
+vim.wo.breakindent = true -- Preserve indentation on wrapped lines
